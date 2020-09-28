ba0cfa336f51200aae3c49c39bb80d3ec6d5b8fc
Triangleyj <dpwns99@gmail.com>
2020-09-28 13:30:42 +0900

    Add frontend vue files

---
+++ frontend/src/views/Post.vue
@@ -0,0 +1,261 @@
+<template>
+  <TheLayout class="post" :key="postId">
+    <template #aside>
+      <TheSidebar />
+    </template>
+
+    <ThePostHeader
+      :post="post"
+      :context="context"
+      :showHidden="showHidden"
+      @archive="archive"
+      @report="report"
+      @vote="vote"
+      @block="block"
+    />
+
+    <ThePostDetail
+      :post="post"
+      :showHidden="showHidden"
+      @archive="archive"
+      @report="report"
+      @vote="vote"
+      @show-hidden="showHidden = true"
+    />
+
+    <ThePostComments
+      :comments="post.comments"
+      :postId="postId"
+      @upload="addNewComment"
+      @update="updateComment"
+      @refresh="refresh"
+    />
+    <ThePostNavigation :post="post" :context="context" />
+    <!-- @TODO: <TheBoard :board="board"/> -->
+  </TheLayout>
+</template>
+
+<script>
+import { archivePost, blockUser, fetchPost, reportPost, unarchivePost, unblockUser, votePost } from '@/api'
+import { fetchWithProgress } from './helper.js'
+import ThePostComments from '@/components/ThePostComments.vue'
+import ThePostDetail from '@/components/ThePostDetail.vue'
+import ThePostHeader from '@/components/ThePostHeader.vue'
+import ThePostNavigation from '@/components/ThePostNavigation.vue'
+import TheBoard from '@/components/TheBoard.vue'
+import TheLayout from '@/components/TheLayout.vue'
+import TheSidebar from '@/components/TheSidebar.vue'
+import axios from 'axios'
+
+export default {
+  name: 'post',
+  props: {
+    postId: { required: true }
+  },
+  data () {
+    return {
+      post: {},
+      showHidden: false
+    }
+  },
+  computed: {
+    /* eslint-disable camelcase */
+    context () {
+      const { from_view, from_page, search_query } = this.$route.query
+      const query = {}
+      if (from_page) {
+        query['page'] = from_page
+      }
+
+      if (search_query) {
+        query['query'] = search_query
+      }
+
+      switch (from_view) {
+        case 'user':
+          return { name: 'user', params: { username: this.$route.query.created_by }, query }
+
+        case 'recent':
+          query.board = 'recent'
+          return { name: 'my-info', query }
+
+        case 'scrap':
+          return { name: 'archive', query }
+
+        case 'all':
+          return { name: 'board', query }
+
+        default:
+          return {
+            name: 'board',
+            params: {
+              boardSlug: this.post.parent_board ? this.post.parent_board.slug : null
+            },
+            query
+          }
+      }
+    }
+  },
+  methods: {
+    async addNewComment (comment) {
+      console.log(this.post)
+      const parent_comment = this.post.comments.find(comm => comm.id === comment.parent_comment)
+
+      // TODO : alert all on one re-reply
+      console.log(comment.parent_article ? this.post.created_by.id : parent_comment.created_by.id)
+      axios.post('http://localhost:3000/bot/alert/' + (comment.parent_article ? this.post.created_by.id : parent_comment.created_by.id), {
+        sender: comment.created_by.profile.nickname,
+        type: 'reply',
+        reply_type : (comment.parent_article ? '' : 're'),
+        content: comment.content,
+        post_id: this.post.id,
+        target_id: comment.id,
+        my_content: (comment.parent_article ? this.post.title : parent_comment.content)
+
+      }).then(function (response) {
+      })
+
+      if (comment.parent_comment) {
+        /* Save the new recomment in local first. */
+        /*
+        const rootComment = this.post.comments.find(parent => parent.id === comment.parent_comment)
+        rootComment.comments = [
+          ...rootComment.comments,
+          comment
+        ]
+        */
+        /* Then fetch data from DB. */
+        return this.refresh()
+      }
+
+      /* Save the new comment in local first. */
+      /*
+      this.post.comments = [
+        ...this.post.comments,
+        comment
+      ]
+      */
+
+      /* Then fetch data from DB. */
+      return this.refresh()
+    },
+
+    async updateComment (update) {
+      if (update.parent_comment) {
+        const rootComment = this.post.comments.find(comment => comment.id === update.parent_comment)
+        const replyIndex = rootComment.comments.findIndex(replyComment => replyComment.id === update.id)
+        if (replyIndex < 0) return
+
+        this.$set(rootComment.comments, replyIndex, update)
+        return
+      }
+
+      const commentIndex = this.post.comments.findIndex(comment => comment.id === update.id)
+      if (commentIndex < 0) return
+
+      this.$set(this.post.comments, commentIndex, update)
+    },
+
+    // @TODO: 매번 refresh 하는게 최선인지는 좀 생각해 봐야할듯
+    async refresh () {
+      await console.log('fetch', this.post)
+      this.post = await fetchPost({ postId: this.postId, context: this.$route.query })
+    },
+
+    async vote ({ id, vote }) {
+      //await votePost(id, vote)
+      await this.refresh()
+    },
+
+    async archive () {
+      if (this.post.my_scrap) {
+        await unarchivePost(this.post.my_scrap.id)
+        this.post.my_scrap = null
+        this.$store.dispatch('dialog/toast', this.$t('unarchived'))
+      } else {
+        const result = await archivePost(this.post.id)
+        this.post.my_scrap = result
+        this.$store.dispatch('dialog/toast', this.$t('archived'))
+      }
+      await this.$store.dispatch('fetchArchivedPosts')
+    },
+
+    async report () {
+      const result = await this.$store.dispatch('dialog/confirm', this.$t('confirm-report'))
+      if (!result) return
+
+      await reportPost(this.post.id)
+      this.$store.dispatch('dialog/toast', this.$t('reported'))
+    },
+
+    async block () {
+      if (!this.post.created_by.is_blocked) {
+        const result = await this.$store.dispatch('dialog/confirm', this.$t('confirm-block'))
+        if (!result) return
+
+        await blockUser(this.post.created_by.id)
+        this.post.created_by.is_blocked = true
+        this.$store.dispatch('dialog/toast', this.$t('blocked'))
+      } else {
+        await unblockUser(this.post.created_by.id)
+        this.post.created_by.is_blocked = false
+        this.$store.dispatch('dialog/toast', this.$t('unblocked'))
+      }
+
+      this.$router.push()
+      /*
+      const [ post ] = await fetchWithProgress([
+        fetchPost({ postId: this.post.id, context: this.$route.query })
+      ])
+      this.post = post
+      */
+    }
+  },
+
+  async beforeRouteEnter ({ params: { postId }, query }, from, next) {
+    const [ post ] = await fetchWithProgress([
+      fetchPost({ postId, context: query })
+    ])
+    next(vm => { vm.post = post })
+  },
+
+  async beforeRouteUpdate ({ params: { postId }, query }, from, next) {
+    const [ post ] = await fetchWithProgress([
+      fetchPost({ postId, context: query })
+    ])
+    this.post = post
+    this.showHidden = false
+    next()
+  },
+
+  components: {
+    TheBoard,
+    TheLayout,
+    ThePostComments,
+    ThePostDetail,
+    ThePostHeader,
+    ThePostNavigation,
+    TheSidebar
+  }
+}
+</script>
+
+<i18n>
+ko:
+  archived: '담아둔 게시글에 담아졌습니다!'
+  unarchived: '담아둔 게시글에서 제거하였습니다!'
+  reported: '신고되었습니다!'
+  blocked: '해당 유저가 차단되었습니다!'
+  unblocked: '해당 유저가 차단해제되었습니다!'
+  confirm-report: '정말로 신고하시겠습니까?'
+  confirm-block: '정말로 차단하시겠습니까?'
+
+en:
+  archived: 'Successfully added to your archive!'
+  unarchived: 'Successfully removed from your archive!'
+  reported: 'Successfully reported!'
+  blocked: 'The user has been blocked!'
+  unblocked: 'The user has been unblocked!'
+  confirm-report: 'Are you really want to report this post?'
+  confirm-block: 'Are you really want to block this user?'
+</i18n>
---
+++ frontend/src/views/Write.vue
@@ -0,0 +1,198 @@
+<template>
+  <TheLayout>
+    <ThePostWrite
+      ref="write"
+      v-if="postFetched"
+      :post="post"
+      :saving="saving"
+      :empty-warnings="emptyWarnings"
+      @save-post="savePost"
+    />
+  </TheLayout>
+</template>
+
+<script>
+import { fetchPost, createPost, updatePost, uploadAttachments } from '@/api'
+import { fetchWithProgress } from './helper.js'
+import TheLayout from '@/components/TheLayout.vue'
+import ThePostWrite from '@/components/ThePostWrite.vue'
+import axios from "axios";
+
+export default {
+  name: 'write',
+  props: [ 'postId' ],
+  data () {
+    return {
+      post: null,
+      saving: false,
+      saved: false,
+      emptyWarnings: []
+    }
+  },
+  computed: {
+    isEditing () {
+      return !!this.postId
+    },
+    postFetched () {
+      return !this.isEditing || // 새로운 글을 작성하는 경우 바로 보여줌
+        (this.isEditing && this.post) // 기존 글을 수정하는 경우 데이터 페치를 기다림
+    }
+  },
+  methods: {
+    async savePost (newArticle) {
+      const { title, boardId, categoryId, isSexual, isSocial, attachments } = newArticle
+
+      axios.post('http://localhost:3000/bot/alert/all', {
+        type: 'new_post',
+        content: title,
+      }).then(function (response) {
+      })
+
+      this.emptyWarnings = []
+      if (title === '') {
+        this.emptyWarnings.push('title')
+      }
+
+      if (this.$refs.write.getContent() === '<p></p>') {
+        this.emptyWarnings.push('content')
+      }
+
+      if (boardId === '') {
+        this.emptyWarnings.push('board')
+      }
+
+      if (categoryId === '$not-set') {
+        this.emptyWarnings.push('category')
+      }
+
+      if (this.emptyWarnings.length > 0) {
+        this.$store.dispatch('dialog/toast', {
+          type: 'warning',
+          text: this.$t('no-empty')
+        })
+
+        return
+      }
+
+      this.saving = true
+
+      const uploadNeededFiles = attachments.filter(item => !item.uploaded)
+      const attachmentUpdate = {}
+      if (attachments) {
+        try {
+          const results = await uploadAttachments(uploadNeededFiles.map(v => v.file))
+
+          uploadNeededFiles.forEach((item, index) => {
+            attachmentUpdate[item.key] = results[index].data
+
+            item.uploaded = true
+            item.key = results[index].data.id
+          })
+        } catch (err) {
+          this.$store.dispatch('dialog/toast', {
+            type: 'error',
+            text: this.$t('attachment-failed')
+          })
+          this.saving = false
+          return
+        }
+      }
+
+      const attachmentIds = attachments.map(item => item.key)
+      this.$refs.write.updateAttachments(attachmentUpdate)
+
+      const content = this.$refs.write.getContent()
+
+      console.log(content)
+
+      newArticle = {
+        title,
+        content,
+        attachments: attachmentIds,
+        parent_topic: categoryId,
+        is_content_sexual: isSexual,
+        is_content_social: isSocial
+      }
+
+      let result
+      try {
+        if (!this.isEditing) {
+          result = await createPost({
+            newArticle,
+            boardId
+          })
+        } else {
+          result = await updatePost({
+            newArticle,
+            postId: this.postId
+          })
+        }
+      } catch (err) {
+        /*
+         * @TODO: 아마 attachment 삭제...?
+         * 근데 그 때 강인이형 말로는 S3 가격 얼마 안 비싸서
+         * 별로 상관 없다고 했던 것 같아용
+         * - swan
+         */
+
+        this.$store.dispatch('dialog/toast', {
+          type: 'error',
+          text: this.isEditing ? this.$t('create-failed') : this.$t('update-failed')
+        })
+        this.saving = false
+        return
+      }
+      this.saving = false
+      this.saved = true
+      window.removeEventListener('beforeunload', this.beforeUnloadHandler)
+      this.$router.push({ name: 'post', params: { postId: result.id } })
+    }
+  },
+  async beforeRouteEnter ({ params: { postId } }, from, next) {
+    // 새로운 글을 작성하는 경우
+    if (!postId) {
+      next() // !주의: next는 한번만 호출돼야 함
+    // 기존 글을 수정하는 경우
+    } else {
+      const [ post ] = await fetchWithProgress([ fetchPost({ postId }) ])
+      next(vm => { vm.post = post })
+    }
+  },
+
+  async beforeRouteLeave (to, from, next) {
+    if (!this.saved) {
+      const response = await this.$store.dispatch('dialog/confirm', this.$t('before-unload'))
+      if (response) next()
+      return
+    }
+    next()
+  },
+  mounted () {
+    this.beforeUnloadHandler = event => {
+      // 대부분의 경우에는 표시되지 않으나 일단 넣어둠
+      event.returnValue = this.$t('before-unload')
+    }
+    window.addEventListener('beforeunload', this.beforeUnloadHandler)
+  },
+  destroyed () {
+    window.removeEventListener('beforeunload', this.beforeUnloadHandler)
+  },
+  components: { TheLayout, ThePostWrite }
+}
+</script>
+
+<i18n>
+ko:
+  no-empty: '빈 칸을 채워주세요.'
+  attachment-failed: '첨부파일 업로드에 실패했습니다. 용량을 다시 한번 확인해주세요.'
+  create-failed: '글 작성에 실패했습니다. 다시 한 번 시도해주세요.'
+  update-failed: '글 수정에 실패했습니다. 다시 한 번 시도해주세요.'
+  before-unload: '이 포스트는 아직 저장되지 않았습니다! 정말 빠져나가시겠습니까?'
+
+en:
+  no-empty: 'Please fill in the empty input.'
+  attachment-failed: 'Failed to upload attachments. Please double check the size of files.'
+  create-failed: 'Failed to write the post. Please try again after a while.'
+  update-failed: 'Failed to update the post. Please try again after a while.'
+  before-unload: 'This post is not saved yet. Are you sure to exit?'
+</i18n>
